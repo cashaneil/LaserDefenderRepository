b19b10eeb8ef95b3b56ead035da24c1168b815f4
cashaneil <neil.casha.e22138@mcast.edu.mt>
2020-10-28 16:27:07 +0100

    Modified script for player

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -15,4 +15,17 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    private void Move()
+    {
+        //var is a generic variable which changes its type according to the value
+        //deltaX is the difference th ePlayer ship moves in the x-axis
+        var deltaX = Input.GetAxis("Horizontal");
+
+        //newXPosition = current pos in x + difference moved in x axis
+        var newXPos = transform.position.x + deltaX;
+
+        //move the player ship on the x-axis only (newXPos)
+        transform.position = new Vector2(newXPos, transform.position.y);
+    }
 }
