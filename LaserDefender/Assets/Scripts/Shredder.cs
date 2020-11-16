1ae8cf720a3bf7e9c3d9d4501e79430362632d0d
cashaneil <neil.casha.e22138@mcast.edu.mt>
2020-11-16 09:50:46 +0100

    Added shredder for lasers + added path of waypoints for enemy ship

---
+++ LaserDefender/Assets/Scripts/Shredder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        
+    }
+
+    //to destroy laser prefabs which 
+    //we called an in-built function/method called OnTriggerEnter2D()
+    private void OnTriggerEnter2D(Collider2D otherObject)// otherObject is a variable which you intialise
+    {
+        Destroy(otherObject.gameObject); //gameObject refers to the laser prefab
+    }
+}
