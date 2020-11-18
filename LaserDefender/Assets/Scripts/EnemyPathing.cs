9dca85b6d9fba56bd3a502cb254dad111394869e
cashaneil <neil.casha.e22138@mcast.edu.mt>
2020-11-18 16:20:22 +0100

    Added Enemy Movement + Wave Config

--- LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypointsList;
+
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    //var shows the next waypoint
+    int wayPointIndex = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -12,12 +17,42 @@ public class EnemyPathing : MonoBehaviour
         //set the position of the enemy ship to the 1st waypoint
         //when transform.position is set before equal sign, it means set to that position
         //but when tranform.position is set AFTER equal sign, it means get that position (in this case, pos of waypoint(0) is waypointList)
-        transform.position = waypointsList[0].transform.position;
+        transform.position = waypointsList[wayPointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove();
+    }
+
+    //takes care of moving Enemy along the path
+    private void EnemyMove()
+    {
+        // 0, 1, 2       <      3
+        if (wayPointIndex < waypointsList.Count)
+        {
+            //set the target position to the next waypoint Position
+            //targetposition: where we want to go
+            var targetPosition = waypointsList[wayPointIndex].transform.position;
+
+            //enemyMovement per frame
+            targetPosition.z = 0f;
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+            //move from the current postion, to targetPosition, at enemyMovement speed
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+
+            //check if we reached targetPosition
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+                //wayPointIndex = wayPointIndex + 1
+            }
+        }
+        //if enemy reached last waypoint
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ LaserDefender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+
+public class WaveConfig : ScriptableObject //scriptable object is an object that you from scrtach using a script
+{
+    //the enemy sprite
+    [SerializeField] GameObject enemyPrefab;
+
+    //the path to to follow
+    [SerializeField] GameObject pathPrefab;
+
+    //time between enemy spawn
+    [SerializeField] float timeBetweenSpawn = 0.5f;
+
+    //random time difference between spawns
+    [SerializeField] float spawnRandomFactor = 0.3f;
+
+    //number of enemies to spawn per wave
+    [SerializeField] int numberOfEnemies = 5;
+
+    //enemiy movment speed
+    [SerializeField] float enemyMoveSpeed = 2f;
+}
