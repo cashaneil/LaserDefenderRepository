bed4db9d864bd8c0e699effababa53061197440f
cashaneil <neil.casha.e22138@mcast.edu.mt>
2020-11-25 16:21:50 +0100

    Spawning Multiple Enemies

---
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //a list of Waves
+    [SerializeField] List<WaveConfig> waveConfigsList;
+
+    //start from wave 0
+    int startingWave = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the currentWave to the 1st wave (0)
+        var currentWave = waveConfigsList[startingWave];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //spawn all enemies into waveToSpawn
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveToSpawn)
+    {
+        for (int enemyCount = 1; enemyCount <= waveToSpawn.GetNumberofEnemies(); enemyCount++)
+        {
+            //spawn the enemy prefab from waveToSpawn
+            //at the position of the first waypoint in path
+            var newEnemy = Instantiate(
+                waveToSpawn.GetEnemyPrefab(),
+                waveToSpawn.GetWaypointsList()[0].transform.position,
+                Quaternion.identity) as GameObject;//Quaternion.identity means to keep the orginal rotation of the object
+
+            yield return new WaitForSeconds(waveToSpawn.GetTimeBetweenSpawns());
+        }
+    }
+}
