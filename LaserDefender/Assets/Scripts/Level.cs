f31b77439e1b8344c12a066c25c9469bb938499e
cashaneil <neil.casha.e22138@mcast.edu.mt>
2021-01-06 16:37:06 +0100

    Added Start Scene + Title, Subtitle + Start & Quit Buttons

---
+++ LaserDefender/Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //this has to be used in this case, for scenes.
+
+public class Level : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("LaserDefender");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0); //in unity build settings, index no. of StartMenu scene is 0
+    }
+
+    public void LoadQuitGame()
+    {
+        //will only work on EXE
+        print("Quitting");
+        Application.Quit();
+    }
+}
